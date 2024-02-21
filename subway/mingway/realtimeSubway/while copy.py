966d34fbd1eb0fd673644c95f8097f70f9c69cfa
dhalsdl12 <dhalsdl12@gmail.com>
2024-02-21 22:06:50 +0900

    a

---
+++ subway/mingway/realtimeSubway/while copy.py
@@ -0,0 +1,34 @@
+import os, json, requests
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+secret_file = os.path.join(BASE_DIR, 'secret.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+base_url = "http://swopenapi.seoul.go.kr/api/subway/"
+subway_key = secrets['SUBWAY_KEY']
+url = base_url + subway_key + "/json/realtimeStationArrival/0/10/"
+subways = ["건대입구", "잠실", "문정", "사당"]
+dic_eng = {
+    "건대입구" : "Konkuk",
+    "잠실" : "Jamsil",
+    "문정" : "Munjeong",
+    "사당" : "Sadang",
+}
+cnt = 0
+#while True:
+print("subway " + str(cnt))
+cnt += 1
+for subway in subways:
+    response = requests.get(url + subway)
+    if response.status_code == 200:
+        data = response.json()
+        if "realtimeArrivalList" in data:
+            with open('subway//mingway//realtimeSubway//' + dic_eng[subway] + '.json', 'w', encoding='utf-8') as f:
+                json.dump(data["realtimeArrivalList"], f, ensure_ascii=False, indent=4)
+        else:
+            print("Error: 'realtimeArrivalList' not found in data")
+    else:
+        print("Error:", response.status_code)
