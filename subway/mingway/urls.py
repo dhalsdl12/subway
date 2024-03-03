be8a953773d74aae13885d8918c631a973c4d770
dhalsdl12 <dhalsdl12@gmail.com>
2024-03-03 18:16:05 +0900

    1차 개발 완료

--- subway/mingway/urls.py
+++ subway/mingway/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from .views import MingSubwayToWork, MingSubwayToHouse
+from .views import WorkKonkuk, WorkJamsil, WorkSadang
+from .views import HomeMunjung, HomeJamsilToKonkuk, HomeJamsilToSadang
 
 urlpatterns = [
     path('subway/work', MingSubwayToWork.as_view(), name='subwayToWork'),
     path('subway/house', MingSubwayToHouse.as_view(), name='subwayToHouse'),
+    path('subway/work/konkuk', WorkKonkuk.as_view(), name='work-konkuk'),
+    path('subway/work/jamsil', WorkJamsil.as_view(), name='work-jamsil'),
+    path('subway/work/sadang', WorkSadang.as_view(), name='work-sadang'),
+    path('subway/home/munjung', HomeMunjung.as_view(), name='home-munjung'),
+    path('subway/home/jamsil-konkuk', HomeJamsilToKonkuk.as_view(), name='home-jamsil-konkuk'),
+    path('subway/home/jamsil-sadang', HomeJamsilToSadang.as_view(), name='home-jamsil-sadang'),
 ]
--- subway/mingway/views.py
+++ subway/mingway/views.py
@@ -88,3 +88,231 @@ class MingSubwayToWork(APIView):
 class MingSubwayToHouse(APIView):
     def get(self, request, format=None):
         return Response("Ming's Subway to house", status=status.HTTP_200_OK)
+    
+
+class WorkKonkuk(APIView):
+    def get(self, request, format=None):
+        BASE_DIR = Path(__file__).resolve().parent.parent
+        secret_file = os.path.join(BASE_DIR, 'secret.json')
+
+        with open(secret_file) as f:
+            secrets = json.loads(f.read())
+
+        subway_infomation = ["Ming's Subway to work"]
+        base_url = "http://swopenapi.seoul.go.kr/api/subway/"
+        subway_key = secrets['SUBWAY_KEY']
+        url = base_url + subway_key + "/json/realtimeStationArrival/0/10/건대입구"
+
+        response = requests.get(url)
+    
+        if response.status_code == 200:
+            infomation = []
+            data = response.json()["realtimeArrivalList"]
+
+            for info in data:
+                subwayId = info['subwayId']
+                trainLineNm = info['trainLineNm']
+
+                if subwayId[-1] == '2':
+                    if '구의' in trainLineNm:
+                        infomation.append({
+                            'barvlDt' : info['barvlDt'],
+                            'arvlMsg2' : info['arvlMsg2'],
+                            'arvlMsg3' : info['arvlMsg3'],
+                        })
+            subway_infomation.append({'Konkuk Station' : infomation})
+            
+            print(subway_infomation)
+            return Response(subway_infomation, status=status.HTTP_200_OK)
+        else:
+            print("Error:", response.status_code)
+
+
+class WorkJamsil(APIView):
+    def get(self, request, format=None):
+        BASE_DIR = Path(__file__).resolve().parent.parent
+        secret_file = os.path.join(BASE_DIR, 'secret.json')
+
+        with open(secret_file) as f:
+            secrets = json.loads(f.read())
+
+        subway_infomation = ["Ming's Subway to work"]
+        base_url = "http://swopenapi.seoul.go.kr/api/subway/"
+        subway_key = secrets['SUBWAY_KEY']
+        url = base_url + subway_key + "/json/realtimeStationArrival/0/10/잠실"
+
+        response = requests.get(url)
+    
+        if response.status_code == 200:
+            infomation = []
+            data = response.json()["realtimeArrivalList"]
+
+            for info in data:
+                subwayId = info['subwayId']
+                trainLineNm = info['trainLineNm']
+
+                if subwayId[-1] == '8':
+                    if '석촌' in trainLineNm or '모란' in trainLineNm:
+                        infomation.append({
+                            'barvlDt' : info['barvlDt'],
+                            'arvlMsg2' : info['arvlMsg2'],
+                            'arvlMsg3' : info['arvlMsg3'],
+                        })
+            subway_infomation.append({'Jamsil Station' : infomation})
+            
+            print(subway_infomation)
+            return Response(subway_infomation, status=status.HTTP_200_OK)
+        else:
+            print("Error:", response.status_code)
+
+
+class WorkSadang(APIView):
+    def get(self, request, format=None):
+        BASE_DIR = Path(__file__).resolve().parent.parent
+        secret_file = os.path.join(BASE_DIR, 'secret.json')
+
+        with open(secret_file) as f:
+            secrets = json.loads(f.read())
+
+        subway_infomation = ["Ming's Subway to work"]
+        base_url = "http://swopenapi.seoul.go.kr/api/subway/"
+        subway_key = secrets['SUBWAY_KEY']
+        url = base_url + subway_key + "/json/realtimeStationArrival/0/10/사당"
+
+        response = requests.get(url)
+    
+        if response.status_code == 200:
+            infomation = []
+            data = response.json()["realtimeArrivalList"]
+
+            for info in data:
+                subwayId = info['subwayId']
+                trainLineNm = info['trainLineNm']
+
+                if subwayId[-1] == '2':
+                    if '방배' in trainLineNm:
+                        infomation.append({
+                            'barvlDt' : info['barvlDt'],
+                            'arvlMsg2' : info['arvlMsg2'],
+                            'arvlMsg3' : info['arvlMsg3'],
+                        })
+            subway_infomation.append({'Sadang Station' : infomation})
+            
+            print(subway_infomation)
+            return Response(subway_infomation, status=status.HTTP_200_OK)
+        else:
+            print("Error:", response.status_code)
+
+
+class HomeMunjung(APIView):
+    def get(self, request, format=None):
+        BASE_DIR = Path(__file__).resolve().parent.parent
+        secret_file = os.path.join(BASE_DIR, 'secret.json')
+
+        with open(secret_file) as f:
+            secrets = json.loads(f.read())
+
+        subway_infomation = ["Ming's Subway to home"]
+        base_url = "http://swopenapi.seoul.go.kr/api/subway/"
+        subway_key = secrets['SUBWAY_KEY']
+        url = base_url + subway_key + "/json/realtimeStationArrival/0/10/문정"
+
+        response = requests.get(url)
+    
+        if response.status_code == 200:
+            infomation = []
+            data = response.json()["realtimeArrivalList"]
+
+            for info in data:
+                subwayId = info['subwayId']
+                trainLineNm = info['trainLineNm']
+
+                if subwayId[-1] == '8':
+                    if '가락시장' in trainLineNm:
+                        infomation.append({
+                            'barvlDt' : info['barvlDt'],
+                            'arvlMsg2' : info['arvlMsg2'],
+                            'arvlMsg3' : info['arvlMsg3'],
+                        })
+            subway_infomation.append({'Munjung Station' : infomation})
+            
+            print(subway_infomation)
+            return Response(subway_infomation, status=status.HTTP_200_OK)
+        else:
+            print("Error:", response.status_code)
+
+
+class HomeJamsilToKonkuk(APIView):
+    def get(self, request, format=None):
+        BASE_DIR = Path(__file__).resolve().parent.parent
+        secret_file = os.path.join(BASE_DIR, 'secret.json')
+
+        with open(secret_file) as f:
+            secrets = json.loads(f.read())
+
+        subway_infomation = ["Ming's Subway to home"]
+        base_url = "http://swopenapi.seoul.go.kr/api/subway/"
+        subway_key = secrets['SUBWAY_KEY']
+        url = base_url + subway_key + "/json/realtimeStationArrival/0/10/잠실"
+
+        response = requests.get(url)
+    
+        if response.status_code == 200:
+            infomation = []
+            data = response.json()["realtimeArrivalList"]
+
+            for info in data:
+                subwayId = info['subwayId']
+                trainLineNm = info['trainLineNm']
+
+                if subwayId[-1] == '2':
+                    if '잠실나루' in trainLineNm:
+                        infomation.append({
+                            'barvlDt' : info['barvlDt'],
+                            'arvlMsg2' : info['arvlMsg2'],
+                            'arvlMsg3' : info['arvlMsg3'],
+                        })
+            subway_infomation.append({'Jamsil To Konkuk Station' : infomation})
+            
+            print(subway_infomation)
+            return Response(subway_infomation, status=status.HTTP_200_OK)
+        else:
+            print("Error:", response.status_code)
+
+
+class HomeJamsilToSadang(APIView):
+    def get(self, request, format=None):
+        BASE_DIR = Path(__file__).resolve().parent.parent
+        secret_file = os.path.join(BASE_DIR, 'secret.json')
+
+        with open(secret_file) as f:
+            secrets = json.loads(f.read())
+
+        subway_infomation = ["Ming's Subway to home"]
+        base_url = "http://swopenapi.seoul.go.kr/api/subway/"
+        subway_key = secrets['SUBWAY_KEY']
+        url = base_url + subway_key + "/json/realtimeStationArrival/0/10/잠실"
+
+        response = requests.get(url)
+    
+        if response.status_code == 200:
+            infomation = []
+            data = response.json()["realtimeArrivalList"]
+
+            for info in data:
+                subwayId = info['subwayId']
+                trainLineNm = info['trainLineNm']
+
+                if subwayId[-1] == '2':
+                    if '잠실새내' in trainLineNm:
+                        infomation.append({
+                            'barvlDt' : info['barvlDt'],
+                            'arvlMsg2' : info['arvlMsg2'],
+                            'arvlMsg3' : info['arvlMsg3'],
+                        })
+            subway_infomation.append({'Jamsil To Sadang Station' : infomation})
+            
+            print(subway_infomation)
+            return Response(subway_infomation, status=status.HTTP_200_OK)
+        else:
+            print("Error:", response.status_code)
